c5b6e70ceefbb6e4d1c68cafec0a336b871d24f2
jocyanno <jocyannovittor@hotmail.com>
2023-05-24 21:01:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+export default function App() {
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de Água',
+    'Paga a conta de Luz',
+    'Ajeitar o Notebook',
+  ]);
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <h1>Cadastrando Usuário</h1>
 
-export default App;
+      <form onSubmit={handleRegister}>
+        <label>Nome da Tarefa:</label><br />
+        <input
+          placeholder='Digite sua tarefa'
+          value = {input}
+          onChange={(e)=> setInput(e.target.value)}
+        /><br/>
+        <button type='submit'>Registrar</button>
+
+      </form>
+
+      <br/><br/>
+      <ul>
+        {tarefas.map( tarefa => (
+            <li 
+            key={tarefa}>
+              {tarefa}
+            </li>
+          ))}
+      </ul><br />
+    </div>
+  )
+}
